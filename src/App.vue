da453534b6d007f6b6e16a64e1aa0709c3296ad9
zumm <jan.plys@gmail.com>
2026-04-03 19:17:42 +0700

    chore: remove dev components

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,7 @@
 import { onErrorCaptured, onMounted } from 'vue'
 import { ModalsContainer } from 'vue-final-modal'
 import { RouterView, useRouter } from 'vue-router'
-import FpsCounter from '@/components/FpsCounter.vue'
 import JackpotModal from '@/components/JackpotModal.vue'
-import ResetLocalStorage from '@/components/ResetLocalStorage.vue'
 import CriticalErrorScreen from '@/components/screens/CriticalErrorScreen.vue'
 import LoadingScreen from '@/components/screens/LoadingScreen.vue'
 import { useClickSessionsUploader } from '@/composables/click-session-uploader'
@@ -40,9 +38,6 @@ onMounted(() => resume())
 </script>
 
 <template>
-  <ResetLocalStorage class="fixed top-0 left-0 z-999" />
-  <FpsCounter class="fixed top-0 right-0 z-999" />
-
   <JackpotModal />
 
   <template v-if="appStore.error == null">
